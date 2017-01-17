1f778079ca4b64892011b330d1d561380e50c4d3
yaseenagag <yaseen.uc@gmail.com>
2017-01-17 15:12:27 -0800

    implement factorial and tests

---
+++ test/factorial_test.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai'
+import factorial from '../src/factorial'
+
+describe('factorial( 100 )', () => {
+
+  it('should be a function', () => {
+    expect(factorial).to.be.a('function')
+  })
+
+  it('returns the factorial of the input', () => {
+    expect(factorial( 5 )).to.equal(120)
+  })
+
+  it('returns the factorial of the input', () => {
+    expect(factorial( 3 )).to.equal(6)
+  })
+})
